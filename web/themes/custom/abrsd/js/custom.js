86ced393283a799d8f9ec1529a7414cb3ba1b3db
Mark McFarlane <mark.mcfarlane@gmail.com>
2023-03-16 22:36:36 -0400

    Change List (JS): 1 - Added core/drupalSettings library as dependency 2 - Implmeneted code to manage navigation within and between pages targetting individual sections to scroll to. (PoC)

--- web/themes/custom/abrsd/js/custom.js
+++ web/themes/custom/abrsd/js/custom.js
@@ -3,16 +3,41 @@
  * Global utilities.
  *
  */
-(function($, Drupal) {
+// (function($, Drupal) {
 
-  'use strict';
+//   'use strict';
 
+//   Drupal.behaviors.abrsd = {
+//     attach: function(context, settings) {
+//         document.addEventListener('DOMContentLoaded', function() {
+//   };
+
+// })(jQuery, Drupal);
+
+(function (Drupal) {
   Drupal.behaviors.abrsd = {
-    attach: function(context, settings) {
+    attach: function (context, settings) {
+      // Your code here.
+      const scrollTo = window.sessionStorage.getItem('scrollTo');
+      if (scrollTo) {
+        window.sessionStorage.removeItem('scrollTo');
+        const target = document.querySelector('#' + scrollTo);
+        if (target) {
+          target.scrollIntoView({ behavior: 'smooth', block: 'center' });
+        }
+      }
 
-      // Custom code here
+      let el = document.querySelector('.navbar-nav');
+      el.addEventListener('click', function (e) {
+        e.preventDefault();
+        if (e.target.tagName === 'A' && e.target.closest('li').classList.contains('dropdown-item')) {
+          const href = e.target.getAttribute('href');
+          // console.log(href.split('#')[1]);
+          window.sessionStorage.setItem('scrollTo', href.split('#')[1]);
+          window.location.href = href.split('#')[0];
+        }
+      });
 
-    }
+    },
   };
-
-})(jQuery, Drupal);
+}(Drupal));

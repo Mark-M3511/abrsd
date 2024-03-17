1d394a0f57b8577614ef5555cf6a176caf5b0b22
Mark McFarlane <mark.mcfarlane@gmail.com>
2024-03-16 23:48:37 -0400

    Refactor carousel initialization in flickity-front.js

--- web/themes/custom/abrsd/js/flickity-front.js
+++ web/themes/custom/abrsd/js/flickity-front.js
@@ -9,20 +9,24 @@
 
     const { behaviors } = Drupal;
 
+    const caoursel = (elem) => {
+        if (!elem) {
+            return;
+        }
+        const flkty = new Flickity(elem, {
+            // options
+            draggable: true,
+            wrapAround: true,
+            autoPlay: false,
+            prevNextButtons: true,
+            pageDots: true,
+        });
+    }
+
     behaviors.abrsd = {
         attach: function (context, settings) {
             const elem = document.querySelector('.blog-carousel');
-            if (elem) {
-                const flkty = new Flickity(elem, {
-                    // options
-                    cellAlign: 'left',
-                    contain: true,
-                    wrapAround: true,
-                    autoPlay: false,
-                    prevNextButtons: true,
-                    pageDots: true,
-                });
-            }
+            caoursel(elem);
         }
     };
 

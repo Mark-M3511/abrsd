b4692a6925cce5a30ca631e1df1e66f0ac2e7677
Mark McFarlane <mark.mcfarlane@gmail.com>
2024-03-16 22:39:38 -0400

    Add flickity-front.js file for custom code

---
+++ web/themes/custom/abrsd/js/flickity-front.js
@@ -0,0 +1,18 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function (Drupal, drupalSettings) {
+
+    'use strict';
+
+    Drupal.behaviors.bootstrap_sass = {
+        attach: function (context, settings) {
+
+            // Custom code here
+
+        }
+    };
+
+})(Drupal, drupalSettings);

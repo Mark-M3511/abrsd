e0a724928b118127beaf6432c0226cc0d3c3b0d4
Mark McFarlane <mark.mcfarlane@gmail.com>
2024-03-31 23:59:54 -0400

    Update ABRSD User Registration package and handler. Remove superfluous comment lines that broke comment parser when registering the handler.

--- web/modules/custom/abrsd_user_registration/src/Plugin/WebformHandler/UserRegistration.php
+++ web/modules/custom/abrsd_user_registration/src/Plugin/WebformHandler/UserRegistration.php
@@ -7,28 +7,21 @@ use Drupal\webform\WebformSubmissionInterface;
 use Drupal\user\Entity\User;
 use Drupal\Core\Language\LanguageManagerInterface;
 use Drupal\Core\Config\ConfigFactoryInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Form submission handler.
- *  The above code is a simple example of a Webform handler that creates a new user entity when a Webform submission is saved. The handler is defined in the annotation of the class, and the postSave method is called when a Webform submission is saved. The createUser method creates a new user entity using the values from the Webform submission.
- *  The  UserRegistrationHandler  class extends  WebformHandlerBase  and implements the  postSave  method. This method is called after the webform submission is saved. The  postSave  method receives the  WebformSubmissionInterface  object and a boolean value indicating whether the submission is an update.
- *  The  postSave  method gets the values from the submission and calls the  createUser  method to create a new user. The  createUser  method creates a new user entity, sets the username, email, and password, and saves the user.
- *  The  UserRegistrationHandler  class is annotated with the  @WebformHandler  annotation. This annotation provides metadata about the handler, such as the ID, label, category, description, cardinality, and results.
- *  The  UserRegistrationHandler  class is defined in the  UserRegistrationHandler.php  file in the  web/modules/custom/abrsd_user_registration/src/Plugin/WebformHandle  directory.
- *  Step 4: Create a webform handler service
- *  To register the webform handler, create a service definition in the  my_module.services.yml  file.
- *  Path: web/modules/custom/abrsd_user_registration/abrsd_user_registration.services.yml
  *
  * @WebformHandler(
  *   id = "abrsd_user_registration",
- *   label = @Translation("ABRSD User Registration Handler"),
- *   category = @Translation("Custom"),
+ *   label = @Translation("ABRSD User Registration"),
+ *   category = @Translation("ABRSD User Registration"),
  *   description = @Translation("Handles user registrations for the ABRSD website."),
  *   cardinality = \Drupal\webform\Plugin\WebformHandlerInterface::CARDINALITY_SINGLE,
  *   results = \Drupal\webform\Plugin\WebformHandlerInterface::RESULTS_PROCESSED,
  * )
  */
-class UserRegistration extends WebformHandlerBase
+final class UserRegistration extends WebformHandlerBase
 {
 
   /**
@@ -67,6 +60,35 @@ class UserRegistration extends WebformHandlerBase
         self::createUser($values);
       }
     }
+  }
+
+
+  /**
+   * Creates a new instance of the UserRegistration class.
+   *
+   * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
+   *   The container interface.
+   * @param array $configuration
+   *   An array of configuration settings.
+   * @param string $plugin_id
+   *   The plugin ID.
+   * @param mixed $plugin_definition
+   *   The plugin definition.
+   *
+   * @return static
+   *   A new instance of the UserRegistration class.
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition)
+  {
+    $language_manager = $container->get('language_manager');
+    $config_factory = $container->get('config.factory');
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $language_manager,
+      $config_factory
+    );
   }
 
   /**

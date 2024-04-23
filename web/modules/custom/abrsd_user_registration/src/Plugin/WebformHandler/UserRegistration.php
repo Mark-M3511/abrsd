af7d0b0290237a6aafd34181816dc2bf76f67bdc
Mark McFarlane <mark.mcfarlane@gmail.com>
2024-04-23 18:23:06 -0400

    Refactor UserRegistration.php to use UserRegistrationHelper for user account creation

--- web/modules/custom/abrsd_user_registration/src/Plugin/WebformHandler/UserRegistration.php
+++ web/modules/custom/abrsd_user_registration/src/Plugin/WebformHandler/UserRegistration.php
@@ -12,6 +12,7 @@ use Psr\Log\LoggerInterface;
 use Drupal\user\Entity\User;
 use Drupal\Core\Session\AccountProxyInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\abrsd_user_registration\Helper\UserRegistrationHelper;
 
 /**
  * Form submission handler.
@@ -32,37 +33,37 @@ final class UserRegistration extends WebformHandlerBase
    * @var \Drupal\Core\Language\LanguageManagerInterface
    *   The language manager service.
    */
-  protected $languageManager;
+  public $languageManager;
 
   /**
    * @var \Drupal\Core\Config\ConfigFactoryInterface $configFactory
    *   The configuration factory service.
    */
-  protected $configFactory;
+  public $configFactory;
 
   /**
    * @var PasswordGeneratorInterface $passwordGenerator
    *   The password generator service used for generating passwords.
    */
-  protected $passwordGenerator;
+  public $passwordGenerator;
 
   /**
    * @var \Psr\Log\LoggerInterface
    *   The logger service.
    */
-  protected $logger;
+  public $logger;
 
   /**
    * @var bool
    *   A flag indicating whether the user already exists.
    */
-  private $userExists;
+  public $userExists;
 
   /**
    * @var \Drupal\Core\Session\AccountProxyInterface
    *   The current user.
    */
-  private $currentUser;
+  public $currentUser;
 
   /**
    * @var \Drupal\file\Entity\File;
@@ -196,16 +197,8 @@ final class UserRegistration extends WebformHandlerBase
         $values['sid'] = $webform_submission->id();
         // If no user is found, create a new user
         if (!$update) {
-          if (!$this->userExists) {
-            $user = $this->createUserAccount($values);
-            if ($user) {
-              $this->logger->info('User created: ' . $user->mail->value);
-              // Send an email to the user of the type 'user_register_pending_approval'
-              _user_mail_notify('register_pending_approval', $user);
-            }
-          } else {
-            $this->logger->info('User already exists with Email address: ' . $email);
-          }
+          $reg_helper = new UserRegistrationHelper($user, $webform_submission, $this);
+          $reg_helper->addUserAccount();
         } else {
           // If the user is authenticated, update the user account else log a message
           if ($user->isAuthenticated()) {
@@ -216,60 +209,6 @@ final class UserRegistration extends WebformHandlerBase
     } catch (\Exception $e) {
       $this->logger->error($e->getMessage());
     }
-  }
-
-  /**
-   * Create a new user.
-   *
-   * @param array $values
-   *   The values from the webform submission.
-   */
-  private function createUserAccount(array $values)
-  {
-    $result = null;
-
-    try {
-      $language_id = $this->languageManager->getCurrentLanguage()->getId();
-      $password = $this->passwordGenerator->generate(30);
-      $email = $values['confirm_email_address'];
-
-      $user_values = [
-        'pass' => $password,
-        'mail' => $email,
-        'name' => $email,
-        'langcode' => $language_id,
-        'init' => $email,
-        'status' => 0,
-        'preferred_langcode' => $language_id,
-        'preferred_admin_langcode' => $language_id,
-        'field_organization' => $values['organization'],
-        'field_interests' => $values['interests'],
-        'field_display_name' => $values['display_name'],
-        'field_first_name' => $values['first_name'],
-        'field_last_name' => $values['last_name'],
-        'field_country' => $values['country'],
-        'field_about_me' => $values['about_me'],
-        'field_webform_submission_id' => $values['sid'],
-      ];
-
-      // Create a new user
-      $user = User::create($user_values);
-      // Set the user role
-      $default_role = 'comment_contributor';
-      $custom_role = $this->configFactory->get('abrsd_user_registration.settings')
-        ->get('roles')[1] ?? $default_role;
-      // Add the custom role to the user
-      $user->addRole($custom_role);
-
-      if ($user->enforceIsNew()->save()) {
-        $result = $user;
-      }
-    } catch (\Exception $e) {
-      $this->logger->error($e->getMessage());
-      // throw $e;
-    }
-
-    return $result;
   }
 
   /**

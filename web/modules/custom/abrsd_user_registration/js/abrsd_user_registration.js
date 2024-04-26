e0545ccd554e05ddbcf9d70101a6ed628ba875f3
Mark McFarlane <mark.mcfarlane@gmail.com>
2024-04-26 04:07:58 -0400

    Refactor abrsd_user_registration.js to remove console log statements and improve profile picture functionality

--- web/modules/custom/abrsd_user_registration/js/abrsd_user_registration.js
+++ web/modules/custom/abrsd_user_registration/js/abrsd_user_registration.js
@@ -7,8 +7,6 @@
                     if (mutation.type === 'childList') {
                         // Check for the presence of a input element with the name attribute = "files[profile_picture]"
                         if (document.querySelector('input[name="files[profile_picture]"]')) {
-                            // Console log it
-                            // console.log('The profile picture input element is present.');
                             // Hide the element with id attribute == #profile-picture-display
                             document.querySelector('#profile-picture-display').style.display = 'none';
                         }
@@ -23,7 +21,7 @@
             observer.observe(document, { attributes: true, childList: true, subtree: true });
 
             // For example, to log a message to the console:
-            console.log('abrsd_user_registration module JavaScript loaded.');
+            // console.log('abrsd_user_registration module JavaScript loaded.');
         }
     };
 })(Drupal, drupalSettings);

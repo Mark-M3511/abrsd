962ce2f36bf6e2b111b6d23d8fc615a73e241bc6
Mark McFarlane <mark.mcfarlane@gmail.com>
2024-04-26 13:34:21 -0400

    Refactor abrsd_user_registration.js to improve code readability and maintainability.

--- web/modules/custom/abrsd_user_registration/js/abrsd_user_registration.js
+++ web/modules/custom/abrsd_user_registration/js/abrsd_user_registration.js
@@ -2,9 +2,9 @@
     Drupal.behaviors.abrsdUserRegistration = {
         attach: function (context, settings) {
             // Create a new MutationObserver instance.
-            const observer = new MutationObserver(function (mutations) {
+            const observer = new MutationObserver(mutations => {
                 const elUploader = document.querySelector('input[name="files[profile_picture]"]');
-                mutations.forEach(function (mutation) {
+                mutations.forEach(mutation => {
                     if (mutation.type === 'childList') {
                         if (elUploader) {
                             // Hide the element with id attribute == #profile-picture-display
@@ -13,8 +13,6 @@
                                 elDisplay.style.display = 'none';
                             }
                         }
-                    } else if (mutation.type === 'attributes') {
-                        // console.log('The ' + mutation.attributeName + ' attribute was modified.');
                     }
                 });
             });

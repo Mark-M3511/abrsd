9aeea2d25c5c678d7eedfd03f94aaa473ba79788
Mark McFarlane <mark.mcfarlane@gmail.com>
2024-06-02 17:54:13 -0400

    refactor: Add form submission loader to profile form and adjust styling
    This commit adds a form submission loader to the profile form in the abrsd_user_registration module. It includes a new JavaScript file, form-submission-loader.js, which handles the form submission event and displays a loader element while the form is being submitted. Additionally, the commit includes a new CSS file, form-spinner.css, which provides styling for the loader element. This change improves the user experience by providing visual feedback during form submission and enhances the overall appearance of the profile form.

---
+++ web/themes/custom/abrsd/js/form-submission-loader.js
@@ -0,0 +1,27 @@
+// Set up a Drupal JS file to handle form submission loader
+/**
+ * @file
+ * Form Submission Loader.
+ */
+((Drupal, drupalSettings) => {
+    'use strict';
+    /**
+     * @type {Drupal}
+     */
+    const { behaviors } = Drupal;
+    /**
+     * Process form submission loader
+     * @param Event e - The form submission event parameter
+     * @returns {void}
+     */
+    behaviors.formSubmissionLoader = {
+        attach(context, settings) {
+            const btn = document.querySelector('.webform-button--submit');
+            btn?.addEventListener('click', () => {
+                const loader = document.querySelector('.loader');
+                // Replace d-none with d-block class
+                loader.classList.replace('d-none', 'd-inline-block');
+            });
+        }
+    };
+})(Drupal, drupalSettings);

a26311b425e6a537f77296b4b579bb5c3fea4440
Mark McFarlane <mark.mcfarlane@gmail.com>
2024-06-04 23:01:16 -0400

    refactor: Update form submission loader to update the create date and time for the Registration form.

--- web/themes/custom/abrsd/js/form-submission-loader.js
+++ web/themes/custom/abrsd/js/form-submission-loader.js
@@ -17,11 +17,28 @@
     behaviors.formSubmissionLoader = {
         attach(context, settings) {
             const btn = document.querySelector('.webform-button--submit');
+            const regForm = document.querySelector('.webform-submission-user-registration-form');
             btn?.addEventListener('click', () => {
                 const loader = document.querySelector('.loader');
                 // Replace d-none with d-block class
                 loader?.classList.replace('d-none', 'd-inline-block');
             });
+            if (regForm) {
+                const formMessage = document.querySelector('.form-message');
+                //  Get the serverTime value from the drupalSettings object
+                const serverTime = drupalSettings.abrsd_user_registration.serverTime;
+                const browserTime = new Date();
+                const options = {
+                    year: 'numeric',
+                    month: 'long',
+                    day: 'numeric',
+                    hour: '2-digit',
+                    minute: '2-digit'
+                };
+                const formattedDate = browserTime.toLocaleString('en-US', options);
+                // Set the text of the span element
+                formMessage.textContent = `Registration form created on: ${formattedDate}`;
+            }
         }
     };
 })(Drupal, drupalSettings);

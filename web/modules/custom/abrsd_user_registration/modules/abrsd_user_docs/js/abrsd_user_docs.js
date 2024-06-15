ca12a9dfb846de7ad2fa26d1a19cde448cd141e1
Mark McFarlane <mark.mcfarlane@gmail.com>
2024-06-15 00:43:02 -0400

    refactor: Improve lazy loading implementation for user docs modal

--- web/modules/custom/abrsd_user_registration/modules/abrsd_user_docs/js/abrsd_user_docs.js
+++ web/modules/custom/abrsd_user_registration/modules/abrsd_user_docs/js/abrsd_user_docs.js
@@ -7,19 +7,23 @@
                     element.classList.add('loadNodeInModal-processed'); // Mark element as processed
                     element.addEventListener('click', function (e) {
                         e.preventDefault();
-                        var nodeUrl = this.getAttribute('href');
+                        const nodeUrl = this.getAttribute('href');
                         // Fetch the content from the URL
                         fetch(nodeUrl)
                             .then(response => response.text())
                             .then(html => {
                                 // Find the modal body and set its content
-                                var modalBody = document.querySelector('#yourModal .modal-body');
-                                if (modalBody) {
-                                    modalBody.innerHTML = html;
-                                    // Show the modal, assuming Bootstrap's modal is initialized in JavaScript
-                                    var modal = new bootstrap.Modal(document.getElementById('yourModal'));
-                                    modal.show();
-                                }
+                                // const modalBody = document.querySelector('#yourModal .modal-body');
+                                // if (modalBody) {
+                                //     modalBody.innerHTML = html;
+                                //     // Show the modal, assuming Bootstrap's modal is initialized in JavaScript
+                                //     var modal = new bootstrap.Modal(document.getElementById('yourModal'));
+                                //     modal.show();
+                                // }
+                                const newElement = document.createElement('div');
+                                // Assign the fetched HTML to the new element
+                                newElement.innerHTML = html;
+                                console.log(newElement.querySelector('.field--name-body').innerHTML);
                             })
                             .catch(error => console.error('Error loading the URL: ', error));
                     });

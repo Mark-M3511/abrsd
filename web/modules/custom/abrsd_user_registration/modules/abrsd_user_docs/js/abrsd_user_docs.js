042be34f778be1fae389b8b277d1f5134d93947d
Mark McFarlane <mark.mcfarlane@gmail.com>
2024-06-15 22:42:50 -0400

    feat: Add lazy loading for user docs modal
    This commit adds lazy loading functionality to the user docs modal. When the user clicks on a link to open the modal, the content is fetched asynchronously from the server, improving page load performance. The code has been updated in the abrsd_user_docs.js file to handle the fetching and rendering of the modal content. Additionally, the abrsd_user_docs.module file has been modified to include the necessary hook implementation for attaching the library to the page attachments when the user is on the 'user/create-member-account' path.

--- web/modules/custom/abrsd_user_registration/modules/abrsd_user_docs/js/abrsd_user_docs.js
+++ web/modules/custom/abrsd_user_registration/modules/abrsd_user_docs/js/abrsd_user_docs.js
@@ -1,34 +1,34 @@
 (function (Drupal, settings) {
-    Drupal.behaviors.abrsdUserRegistration = {
+    Drupal.behaviors.abrsdUserDocs = {
         attach: function (context, settings) {
-            document.addEventListener('DOMContentLoaded', function () {
-                // Ensure the code runs only once per element
-                document.querySelectorAll('.open-modal:not(.loadNodeInModal-processed)').forEach(function (element) {
-                    element.classList.add('loadNodeInModal-processed'); // Mark element as processed
-                    element.addEventListener('click', function (e) {
-                        e.preventDefault();
-                        const nodeUrl = this.getAttribute('href');
-                        // Fetch the content from the URL
-                        fetch(nodeUrl)
-                            .then(response => response.text())
-                            .then(html => {
-                                // Find the modal body and set its content
-                                // const modalBody = document.querySelector('#yourModal .modal-body');
-                                // if (modalBody) {
-                                //     modalBody.innerHTML = html;
-                                //     // Show the modal, assuming Bootstrap's modal is initialized in JavaScript
-                                //     var modal = new bootstrap.Modal(document.getElementById('yourModal'));
-                                //     modal.show();
-                                // }
-                                const newElement = document.createElement('div');
-                                // Assign the fetched HTML to the new element
-                                newElement.innerHTML = html;
-                                console.log(newElement.querySelector('.field--name-body').innerHTML);
-                            })
-                            .catch(error => console.error('Error loading the URL: ', error));
-                    });
+            // Ensure the code runs only once per element
+            document.querySelectorAll('.open-modal:not(.loadNodeInModal-processed)').forEach(function (element) {
+                element.classList.add('loadNodeInModal-processed'); // Mark element as processed
+                element.addEventListener('click', function (e) {
+                    e.preventDefault();
+                    const nodeUrl = this.getAttribute('href');
+                    // Fetch the content from the URL
+                    fetch(nodeUrl)
+                        .then(response => response.text())
+                        .then(html => {
+                            const newElement = document.createElement('div');
+                            // Assign the fetched HTML to the new element
+                            newElement.innerHTML = html;
+                            const docContent = newElement.querySelector('.field--name-body').innerHTML
+                            // Find the modal body and set its content
+                            const modalBody = document.querySelector('#docsModal .modal-body');
+                            if (modalBody && docContent) {
+                                modalBody.innerHTML = docContent;
+                                // Show the modal, assuming Bootstrap's modal is initialized in JavaScript
+                                const modal = new bootstrap.Modal(document.querySelector('#docsModal'));
+                                modal.show();
+                            }
+                            console.log(newElement.querySelector('.field--name-body').innerHTML);
+                        })
+                        .catch(error => console.error('Error loading the URL: ', error));
                 });
             });
+
         }
     };
 })(Drupal, drupalSettings);

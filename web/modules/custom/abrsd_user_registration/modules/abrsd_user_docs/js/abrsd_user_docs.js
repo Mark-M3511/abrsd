f8a1b17219a9c320da015d6af3dd71cc2c2fb494
Mark McFarlane <mark.mcfarlane@gmail.com>
2024-06-16 00:40:43 -0400

    feat: Add data attribute with node URL to user docs modal
    The code changes in this commit add a data attribute to the user docs modal in the `abrsd_user_docs.js` file. This data attribute stores the path of the node associated with the modal content. Additionally, an event listener is added to the modal to execute code after the modal is closed. This enhancement improves the functionality and usability of the user docs modal.

--- web/modules/custom/abrsd_user_registration/modules/abrsd_user_docs/js/abrsd_user_docs.js
+++ web/modules/custom/abrsd_user_registration/modules/abrsd_user_docs/js/abrsd_user_docs.js
@@ -22,13 +22,26 @@
                             if (modalBody && docContent) {
                                 modalTitle.innerHTML = `You are required to read and acknowledge the ${docTitle}`;
                                 modalBody.innerHTML = docContent;
+                                // Add a data attribute with the path of the node
+                                modalBody.setAttribute('data-node-url', nodeUrl);
                                 // Show the modal, assuming Bootstrap's modal is initialized in JavaScript
-                                const modal = new bootstrap.Modal(document.querySelector('#docsModal'));
+                                const modal = new bootstrap.Modal(document.querySelector('#docsModal'),
+                                    {
+                                        keyboard: false,
+                                        backdrop: 'static'
+                                    });
                                 modal.show();
                             }
                         })
                         .catch(error => console.error('Error loading the URL: ', error));
                 });
+            });
+            // Get the modal element
+            const docsModal = document.querySelector('#docsModal');
+            // Add event listener for when the modal is closed
+            docsModal.addEventListener('hidden.bs.modal', function (event) {
+                // Code to execute after the modal is closed
+                console.log('Modal has been closed');
             });
 
         }

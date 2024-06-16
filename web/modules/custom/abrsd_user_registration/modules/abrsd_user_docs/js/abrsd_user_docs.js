28995d39db9ca9d7b681d9361184c1ac6ebb86c2
Mark McFarlane <mark.mcfarlane@gmail.com>
2024-06-15 23:03:55 -0400

    refactor: Update modal title text with a "lead-in" phrase.

--- web/modules/custom/abrsd_user_registration/modules/abrsd_user_docs/js/abrsd_user_docs.js
+++ web/modules/custom/abrsd_user_registration/modules/abrsd_user_docs/js/abrsd_user_docs.js
@@ -15,9 +15,12 @@
                             // Assign the fetched HTML to the new element
                             newElement.innerHTML = html;
                             const docContent = newElement.querySelector('.field--name-body').innerHTML
+                            const docTitle = newElement.querySelector('.field--name-body h2').innerText;
                             // Find the modal body and set its content
                             const modalBody = document.querySelector('#docsModal .modal-body');
+                            const modalTitle = document.querySelector('#docsModal .modal-title');
                             if (modalBody && docContent) {
+                                modalTitle.innerHTML = `You are required to read and acknowledge the ${docTitle}`;
                                 modalBody.innerHTML = docContent;
                                 // Show the modal, assuming Bootstrap's modal is initialized in JavaScript
                                 const modal = new bootstrap.Modal(document.querySelector('#docsModal'));

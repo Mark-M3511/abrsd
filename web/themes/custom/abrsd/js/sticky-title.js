3923890a3edee2608e25f595edf596d3b36c7481
Mark McFarlane <mark.mcfarlane@gmail.com>
2024-06-17 23:15:43 -0400

    refactor: Improve sticky title behavior and add intersection observer
    This commit refactors the sticky title behavior in the sticky-title.js file. It improves the code so that it now checks if the sticky title element exists before applying any changes. This change enhances the functionality and performance of the sticky title feature.

--- web/themes/custom/abrsd/js/sticky-title.js
+++ web/themes/custom/abrsd/js/sticky-title.js
@@ -21,23 +21,24 @@
       const navBarTop = document.querySelector('#navbar-top');
 
       const top = navbarMain?.clientHeight;
-      // Set the top position of the sticky title with !important
-      stickyTitle.style.top = `${top}px`;
+      if (stickyTitle) {
+        // Set the top position of the sticky title with !important
+        stickyTitle.style.top = `${top}px`;
+        // Create an intersection observer
+        const observer = new IntersectionObserver(function (entries) {
+          // If the element is intersecting (visible in the viewport)
+          if (entries[0].isIntersecting === true) {
+            // console.log('Element is in the viewport');
+            stickyTitle.classList.remove('reveal');
+          } else {
+            // console.log('Element has left the viewport');
+            stickyTitle.classList.add('reveal');
+          }
+        }, { threshold: [0.5] });
 
-      // Create an intersection observer
-      const observer = new IntersectionObserver(function (entries) {
-        // If the element is intersecting (visible in the viewport)
-        if (entries[0].isIntersecting === true) {
-          // console.log('Element is in the viewport');
-          stickyTitle.classList.remove('reveal');
-        } else {
-          // console.log('Element has left the viewport');
-          stickyTitle.classList.add('reveal');
-        }
-      }, { threshold: [0.5] });
-
-      // Start observing the element
-      observer.observe(title);
+        // Start observing the element
+        observer.observe(title);
+      }
     }
   };
 

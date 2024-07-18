785562365262aee9dd94f0182b88879d6cfb380b
Mark McFarlane <mark.mcfarlane@gmail.com>
2024-07-18 14:07:31 -0400

    refactor: Improve user profile image handling and API interaction
    - Update abrsd_user_info.js to improve user profile image handling and API interaction.
    - Refactor the code to use the 'user-initial' class for profile images instead of the generic 'align-self-center' class.

--- web/modules/custom/abrsd_user_registration/js/abrsd_user_info.js
+++ web/modules/custom/abrsd_user_registration/js/abrsd_user_info.js
@@ -1,51 +1,68 @@
 (function (Drupal, settings) {
-    Drupal.behaviors.abrsdUserInfo = {
+    const { behaviors } = Drupal;
+    behaviors.abrsdUserInfo = {
         attach: function (context, settings) {
             console.log('abrsdUserInfo');
             // #comment-13 > div.comment__meta.d-none.d-lg-block.col-sm-2 > div > article > div > a > img
-            const img = document.querySelector('.field--name-user-picture img');
-            img?.addEventListener('click', function (e) {
+            // const img = document.querySelector('.field--name-user-picture img');
+            const comment = document.querySelector('.field--name-field-blog-comment');
+            comment?.addEventListener('click', function (e) {
                 e.preventDefault();
-                getUserDataFromAPI();
+                const target = e.target.classList.contains('image-style-thumbnail')
+                    || e.target.classList.contains('user-initials')
+                    || e.target.classList.contains('user-initial');
+                // If the click target is a profile image or user name, get the user data
+                if (target) {
+                    const userId = e.target.getAttribute('data-source-id')
+                        || e.target.closest('[data-source-id]').getAttribute('data-source-id');
+                    if (userId) {
+                        behaviors.abrsdUserInfo.getUserDataFromAPI(userId);
+                    } else {
+                        console.error('User profile not found');
+                    }
+                }
             });
+        },
+        getUserDataFromAPI: function (userId) {
+            // Define the user ID and API endpoint URL
+            // const userId = 'e87fd47d-ccb8-4237-b2bd-8bf36be678e0'; // Replace with the actual user ID
+            const apiUrl = `/jsonapi/user/user/${userId}`;
+
+            // Basic authentication credentials
+            const username = '3f1d9b2e-4e2f-4a2b-9e8e-60a3a7e1b3c6';
+            const password = 'u7Wg7k2qsmsqRwX7rKNlDgtevmKO3iO4wAYdQOFz4929756KJBATe3p95bP0xWwZ';
+            const authHeader = 'Basic ' + btoa(`${username}:${password}`);
+
+            // Fetch user data from the Drupal JSON API
+            fetch(apiUrl, {
+                method: 'GET',
+                headers: {
+                    'Authorization': authHeader,
+                    'Accept': 'application/vnd.api+json'
+                }
+            })
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok ' + response.statusText);
+                    }
+                    return response.json();
+                })
+                .then(responseData => {
+                    // console.log('User data:', data);
+                    const { name, created, field_about_me } = responseData.data.attributes;
+                    const message = `Name: ${name}\nMember since: ${behaviors.abrsdUserInfo.formatDate(created)}` +
+                        `\nAbout Me: ${field_about_me ?? 'N/A'}`;
+                    console.log(message);
+                })
+                .catch(error => {
+                    console.error('There was a problem with the fetch operation:', error);
+                });
+        },
+        formatDate: function formatDate(dateString) {
+            const date = new Date(dateString);
+            const options = { year: 'numeric', month: 'long', day: 'numeric' };
+            return date.toLocaleDateString('en-US', options);
         }
     };
 
-    function getUserDataFromAPI() {
-        // Define the user ID and API endpoint URL
-        const userId = 'e87fd47d-ccb8-4237-b2bd-8bf36be678e0'; // Replace with the actual user ID
-        const apiUrl = `/jsonapi/user/user/${userId}`;
-
-        // Basic authentication credentials
-        const username = 'bsaka@arsenal.com';
-        const password = 'u7Wg7k2qsmsqRwX7rKNlDgtevmKO3iO4wAYdQOFz4929756KJBATe3p95bP0xWwZ';
-        const authHeader = 'Basic ' + btoa(`${username}:${password}`);
-
-        // Fetch user data from the Drupal JSON API
-        fetch(apiUrl, {
-            method: 'GET',
-            headers: {
-                'Authorization': authHeader,
-                'Accept': 'application/vnd.api+json'
-            }
-        })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok ' + response.statusText);
-                }
-                return response.json();
-            })
-            .then(data => {
-                // console.log('User data:', data);
-                const user = data.data.attributes;
-                const message = `Name: ${user.name}\nEmail: ${user.mail}\nCreated: ${user.created}`+
-                    `\nAbout Me: ${user.field_about_me}`;
-                // Destructure the attributes property to get the user data
-                // const { name, mail, created } = data.data.attributes;
-                console.log(message);
-            })
-            .catch(error => {
-                console.error('There was a problem with the fetch operation:', error);
-            });
-    }
 })(Drupal, drupalSettings);
